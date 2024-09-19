25a1250cb408cfe1b2d4845a39e42793097a0fd6
Praveen Perera <me@praveenperera.com>
2024-09-19 17:03:00 -0500

    Add example to README

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # PubPort
 
+<!-- cargo-rdme start -->
+
 A tool to import a wallet public key with descriptors from many different formats seamlessly
+
 
 ## Supported formats
 
@@ -12,3 +15,19 @@ A tool to import a wallet public key with descriptors from many different format
 ## Supported descriptors
 
 - Single Sig
+
+## Example
+
+```rust
+use pubport::Format;
+
+let string = std::fs::read_to_string("test/data/coldcard-export.json").unwrap();
+let format = Format::try_new_from_str(&string);
+
+assert!(format.is_ok());
+
+let format = format.unwrap();
+matches!(format, Format::Json(_));
+```
+
+<!-- cargo-rdme end -->
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,31 @@
+//! A tool to import a wallet public key with descriptors from many different formats seamlessly
+//!
+//!
+//! # Supported formats
+//!
+//! - Descriptors
+//! - Electrum
+//! - Wasabi
+//! - JSON
+//!
+//! # Supported descriptors
+//!
+//! - Single Sig
+//!
+//! # Example
+//!
+//! ```rust
+//! use pubport::Format;
+//!
+//! let string = std::fs::read_to_string("test/data/coldcard-export.json").unwrap();
+//! let format = Format::try_new_from_str(&string);
+//!
+//! assert!(format.is_ok());
+//!
+//! let format = format.unwrap();
+//! matches!(format, Format::Json(_));
+//! ```
+
 pub mod descriptors;
 pub mod formats;
 pub mod json;
